6907b0d3879a51fd6aae8698299c988b7a9cc938
manuvilla86 <manuvilla86@gmail.com>
2026-07-21 20:14:50 -0300

    fix: add DELETE to CORS allowed methods for uninstall endpoint

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func main() {
 func cors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusNoContent)
